cf7750bf9a6ca1de542122f630bf3b64707038b7
srmward <srmward@gmail.com>
2019-08-10 18:31:46 -0700

    Finish API Work

--- backend/src/users/User.ts
+++ backend/src/users/User.ts
@@ -19,34 +19,39 @@ export interface UserDocument extends mongoose.Document {
 /**
  * Schema for User
  */
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
       }
-    }
-  ]
-})
+    ]
+  },
+  {
+    timestamps: true
+  }
+)
 
 /**
  * Add virtual property for relation with tasks
--- backend/src/users/UserRouter.ts
+++ backend/src/users/UserRouter.ts
@@ -6,12 +6,40 @@ import * as UserController from './UserController'
  * Register routes for User
  */
 export const register = (router: Router): Router => {
+  /**
+   * Get all users
+   */
   router.get('/users', UserController.getUsers)
+
+  /**
+   * Get a user's details
+   */
   router.get('/users/me', auth, UserController.getUserDetails)
 
+  /**
+   * Add a new user
+   */
   router.post('/users', UserController.addUser)
+
+  /**
+   * Log a user in
+   */
   router.post('/users/login', UserController.loginUser)
+
+  /**
+   * Log a user out
+   */
   router.post('/users/logout', auth, UserController.logoutUser)
+
+  /**
+   * Update a user
+   */
+  router.put('/users/:id', UserController.updateUser)
+
+  /**
+   * Delete a user
+   */
+  router.delete('/users/:id', UserController.deleteUser)
 
   return router
 }
