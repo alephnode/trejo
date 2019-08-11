ed3ef5dbfb9cd94f7fa89f5ba1aadec08205477a
srmward <srmward@gmail.com>
2019-08-10 17:52:58 -0700

    Update TaskRouter, Scaffold the cli

--- backend/src/tasks/TaskRouter.ts
+++ backend/src/tasks/TaskRouter.ts
@@ -1,11 +1,29 @@
 import { Router } from 'express'
-import { getTasks, addTask } from './TaskController'
+import * as TaskController from './TaskController'
 
 /**
  * Register routes for Task
  */
 export const register = (router: Router): Router => {
-  router.get('/tasks', getTasks)
-  router.post('/tasks', addTask)
+  /**
+   * Get all tasks
+   */
+  router.get('/tasks', TaskController.getTasks)
+
+  /**
+   * Add a new task
+   */
+  router.post('/tasks', TaskController.addTask)
+
+  /**
+   * Update an existing task
+   */
+  router.put('/tasks/:id', TaskController.updateTask)
+
+  /**
+   * Delete a task
+   */
+  router.delete('/tasks/:id', TaskController.deleteTask)
+
   return router
 }
